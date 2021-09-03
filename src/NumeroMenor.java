ca32e174a6175389590e0b2a4c55606502369f07
Marcos Santos <89051454+MarcosAntfon@users.noreply.github.com>
2021-09-03 16:31:02 -0300

    segunda
    aula

---
+++ src/NumeroMenor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+
+public class NumeroMenor { 
+	
+	public static void main(String args[]) { 
+	
+		Scanner leia=new Scanner(System.in); 
+		
+		int n1 = 0 ;
+		int n2 = 0 ; 
+		
+		System.out.println("Digite o 1º numero: "); 
+		n1 = leia.nextInt(); 
+		
+		System.out.println("Digite o 2º numero: "); 
+		n2 = leia.nextInt(); 
+			
+			System.out.println(n2+", "+n1); 
+		
+			System.out.println(n1+", "+n2); 
+
+	}
+	
+	}
